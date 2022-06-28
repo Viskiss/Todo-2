be7fbb90bbca36f4704cfba004de388c159b33bc
Viskiss <98615844+Viskiss@users.noreply.github.com>
2022-06-28 21:17:54 +0300

    update

--- input.js
+++ input.js
@@ -5,7 +5,8 @@ const UL_ACTIVE = document.querySelector(".ul_active");
 const UL_COMPLETED = document.querySelector(".ul_completed");
 const UL_SET = document.querySelector(".ul_set");
 
-const LI = document.querySelectorAll("li");
+// const INPUT = document.querySelector(".input")
+const UL = document.querySelectorAll(".ul_active .ul_completed .ul_set");
 
 const LOCAL_STORAGE_TODO = "todos";
 
@@ -39,29 +40,43 @@ FORM.addEventListener("submit", (event) => {
 UL_ACTIVE.addEventListener("click", (event) => {
   const btnType = event.target.dataset.btn;
   const id = event.target.dataset.todoId;
-
+  const input = document.querySelector(`input[data-todo-id="${id}"]`);
+  console.log(input);
   if (btnType === "set") {
     newtodo = TODOS.find((todo) => todo.id === id);
     newtodo.reserved = true;
+    render();
   } else if (btnType === "completed") {
     newtodo = TODOS.find((todo) => todo.id === id);
     newtodo.completed = true;
+    render();
   } else if (btnType === "delete") {
     newtodo = TODOS.filter((todo) => todo.id !== id);
     TODOS = newtodo;
     console.log(newtodo);
+    render();
+  } else if (btnType === "save") {
+    newtodo = TODOS.find((todo) => todo.id === id);
+    newtodo.value = input.value;
+    console.log(newtodo);
+    render();
   }
-  render();
   save();
 });
 
 UL_COMPLETED.addEventListener("click", (event) => {
   const btnType = event.target.dataset.btn;
   const id = event.target.dataset.todoId;
+  const input = document.querySelector(".input");
   if (btnType === "delete") {
     newtodo = TODOS.filter((todo) => todo.id !== id);
     TODOS = newtodo;
     console.log(newtodo);
+    render();
+    save();
+  } else if (btnType === "save") {
+    newtodo = TODOS.find((todo) => todo.id === id);
+    newtodo.value = input.value;
     render();
     save();
   }
@@ -70,21 +85,30 @@ UL_COMPLETED.addEventListener("click", (event) => {
 UL_SET.addEventListener("click", (event) => {
   const btnType = event.target.dataset.btn;
   const id = event.target.dataset.todoId;
+  const input = document.querySelector(`input[data-todo-id="${id}"]`);
   if (btnType === "active") {
     newtodo = TODOS.find((todo) => todo.id === id);
     newtodo.reserved = false;
     newtodo.completed = false;
+    render();
   } else if (btnType === "completed") {
     newtodo = TODOS.find((todo) => todo.id === id);
     newtodo.completed = true;
     newtodo.reserved = false;
+    render();
   } else if (btnType === "delete") {
     newtodo = TODOS.filter((todo) => todo.id !== id);
     TODOS = newtodo;
     console.log(newtodo);
+    render();
+  } else if (btnType === "save") {
+    newtodo = TODOS.find((todo) => todo.id === id);
+    newtodo.value = input.value;
+    console.log(newtodo);
+    render();
+
+    save();
   }
-  render();
-  save();
 });
 
 DROPDOWN.addEventListener("click", (event) => {
@@ -113,6 +137,46 @@ DROPDOWN.addEventListener("click", (event) => {
     render();
   }
 });
+
+// function TodoEdit(li, id, todo) {
+//   const input = document.querySelector(".input");
+//   li.classList.add("edit");
+//   li.appendChild(input);
+//   input.value = textContent = todo.value;
+//   input.focus();
+//   input.setAttribute("spellcheck", false);
+//   input.addEventListener("blur", (event) => {
+//     if (input.getAttribute("flag") !== "1") {
+//       EditDone(li, todo, input, id);
+//     }
+//   });
+
+//   input.addEventListener("keydown", (event) => {
+//     input.setAttribute("flag", "1");
+//     if (event.keyCode === 13) {
+//       EditDone(li.todo, input, id);
+//     } else if (event.keyCode === 27) {
+//       li.classList.remove("edit");
+//       li.removeChild(input);
+//       save();
+//       render();
+//     }
+//     console.log(input);
+//     input.removeAttribute("flag");
+//   });
+// }
+
+// function EditDone(li, todo, input, id) {
+//   if (input.value.trim() === "") {
+//     TODOS = TODOS.filter((todo) => todo.id !== id);
+//   }
+
+//   li.classList.remove("edit");
+//   todo.value = input.value.trim();
+//   li.removeChild(input);
+//   save();
+//   render();
+// }
 
 const createTodo = (value, selector = "") => {
   const todo = {
@@ -144,6 +208,8 @@ const render = () => {
     li.classList.add("li");
     const div = document.createElement("div");
     div.classList.add("div");
+    const input = document.createElement("input");
+    input.classList.add("input");
 
     const btn_delete = document.createElement("button");
     btn_delete.dataset.btn = "delete";
@@ -166,33 +232,45 @@ const render = () => {
     btn_set.dataset.todoId = todo.id;
     btn_set.classList.add("btn_set", "div_btn");
 
+    const bnt_save = document.createElement("button");
+    bnt_save.dataset.btn = "save";
+    bnt_save.dataset.todoId = todo.id;
+    bnt_save.classList.add("btn_save", "div_btn");
+
     btn_delete.textContent = "delete";
     btn_completed.textContent = "completed";
     btn_set.textContent = "set";
     btn_active.textContent = "active";
+    bnt_save.textContent = "save";
 
-    li.textContent = todo.value;
+    // li.textContent = todo.value;
     li.dataset.todoId = todo.id;
+    input.dataset.todoId = todo.id;
+    input.value = todo.value;
+    // input.setAttribute("readonly", true);
 
     // div.append(btn_delete, btn_selector);
     // li.appendChild(div);
 
     if (!todo.completed && !todo.reserved) {
       UL_ACTIVE.appendChild(li);
-      li.appendChild(div);
-      div.append(btn_delete, btn_set, btn_completed);
+      li.appendChild(input);
+      UL_ACTIVE.appendChild(div);
+      div.append(bnt_save, btn_delete, btn_set, btn_completed);
     }
 
     if (todo.completed) {
       UL_COMPLETED.appendChild(li);
-      li.appendChild(div);
-      div.append(btn_delete);
+      li.appendChild(input);
+      UL_COMPLETED.appendChild(div);
+      div.append(bnt_save, btn_delete);
     }
 
     if (todo.reserved) {
       UL_SET.appendChild(li);
-      li.appendChild(div);
-      div.append(btn_delete, btn_active, btn_completed);
+      li.appendChild(input);
+      UL_SET.appendChild(div);
+      div.append(bnt_save, btn_delete, btn_active, btn_completed);
     }
   });
 };
